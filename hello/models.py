db8278bfa233be7750c8c617a8ab9799887ba7a7
ANDRE AUGUSTO DE ALMEIDA MACHADO <49785448+andrealmac@users.noreply.github.com>
2022-03-17 10:43:55 -0300

    Criado e Inserindo dados nas tabelas do banco de dados

--- hello/models.py
+++ hello/models.py
@@ -1,10 +1,24 @@
+from time import timezone
 from django.db import models
+from django.db import migrations
+
+class Migration(migrations.Migration):
+    atomic = False
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     
+    def __str__(self):
+        return self.question_text
+    #saber se a data de publicacao e maior ou menor que um dia 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now()-datetime.timedelta(days=1)
+    
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
